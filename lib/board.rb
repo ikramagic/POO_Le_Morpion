e0f75022bead56c34db020876bdd3ecc17a608cd
ikramiste <ikrame.saadi@gmail.com>
2023-10-19 11:51:01 +0200

    victory checks if victory is true

--- lib/board.rb
+++ lib/board.rb
@@ -5,14 +5,14 @@ class Board
     attr_accessor :board
 
     def initialize
-      @board = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
+      @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     end
 
     def display_board
       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-      puts "---------"
+      puts "------------"
       puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-      puts "---------"
+      puts "------------"
       puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
     end
 
@@ -22,4 +22,26 @@ class Board
         choice = gets.chomp.to_i
         @board[choice] = player.symbol
     end
+
+    def victory?(player)
+
+      winning_combinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]            
+      ]
+    
+      winning_combinations.each do |combo| #on va boucler sur chacune des sous-boites de winning_comb qui deviennent combo
+        if combo.all? { |position| @board[position] == player.symbol }
+        #{} commence par vérifier les sous-cases = elle contiennent le symbole ?
+        #all? permets scanner si TOUTES les cases correspondent bien à ce qui a été vérifié dans {}
+          puts "Bravo #{player.name}, tu as gagné!"
+          return true
+        end
+      end
+      
+      @board.each do |board_box|
+        if board_box.all?
+    end
+  end
 end
