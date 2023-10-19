cf39093695dc2b76fd04c1326d8d22816093da19
ikramiste <ikrame.saadi@gmail.com>
2023-10-19 15:17:56 +0200

    added array to store all player instances

--- lib/player.rb
+++ lib/player.rb
@@ -3,6 +3,7 @@ require_relative 'game'
 
 class Player
     attr_accessor :name, :symbol
+    @@all_players = []
 
     def initialize
         puts "Choisis un nom :"
@@ -11,5 +12,10 @@ class Player
         puts "Choisis un symbole (x ou o) :"
         symbol = gets.chomp
         @symbol =  symbol
+        @@all_players << self
+    end
+
+    def self.all_players
+        return @@all_players
     end
 end
